5534dce881acef8e6c56e008da83e60feed80690
Gusted <williamzijl7@hotmail.com>
2021-02-20 22:33:13 +0100

    Fix proxy function

--- api/proxy.js
+++
@@ -1,9 +0,0 @@
-const httpProxy = require('http-proxy');
-const corsAnywhereIframe = require('cors-anywhere-iframe');
-const proxyHandler = corsAnywhereIframe.getHandler({}, httpProxy.createServer());
-
-module.exports = (req, res) => {
-    console.log(req.url);
-    req.url = req.url.slice(11);
-    proxyHandler(req, res);
-}
---
+++ api/proxy.ts
@@ -0,0 +1,11 @@
+import { NowRequest, NowResponse } from '@vercel/node'
+import httpProxy from 'http-proxy';
+import corsAnywhereIframe from 'cors-anywhere-iframe';
+
+const proxyHandler = corsAnywhereIframe.getHandler({} as corsAnywhereIframe.CorsAnywhereOptions, httpProxy.createServer());
+
+
+export default function (req: NowRequest, res: NowResponse) {
+    req.url = decodeURIComponent(req.url).substring(11);
+    proxyHandler(req, res);
+}
