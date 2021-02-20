2d54d8ddc46490dbc358052566e9e21a29450d6c
Gusted <williamzijl7@hotmail.com>
2021-02-20 22:34:11 +0100

    Add debug

--- api/proxy.ts
+++ api/proxy.ts
@@ -4,8 +4,8 @@ import corsAnywhereIframe from 'cors-anywhere-iframe';
 
 const proxyHandler = corsAnywhereIframe.getHandler({} as corsAnywhereIframe.CorsAnywhereOptions, httpProxy.createServer());
 
-
 export default function (req: NowRequest, res: NowResponse) {
     req.url = decodeURIComponent(req.url).substring(11);
+    console.log(req.url);
     proxyHandler(req, res);
 }
