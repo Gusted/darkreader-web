378b6ea9c2fdade9a250efe2cac2f1597cbd9473
Gusted <williamzijl7@hotmail.com>
2021-02-21 00:52:37 +0100

    Working submit button

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,11 @@ const setup = () => {
         newScript.textContent = '';
         IFrameDocument.head.append(newScript);
     });
+    const submitButton = document.querySelector('.submit') as HTMLButtonElement;
+    submitButton.addEventListener('click', () => {
+        //TO-DO Normalize & Validate URL.
+        window.location.href = `${window.location.origin}?url=${(document.querySelector('.search-bar') as HTMLInputElement).value}`
+    });
     const parsedURL = new URL(window.location.href);
     if (!parsedURL.searchParams.get('url')) {
         return;
@@ -15,7 +20,7 @@ const setup = () => {
     const workingURL = parsedURL.searchParams.get('url');
     const IFrameSiteWrapper = document.querySelector('.site-wrapper') as HTMLIFrameElement;
     IFrameSiteWrapper.src = `${window.location.origin}/api/proxy?${workingURL}`;
-    const searchBar = document.querySelector('.search-bar') as HTMLButtonElement;
+    const searchBar = document.querySelector('.search-bar') as HTMLInputElement;
     searchBar.value = workingURL;
 };
 
