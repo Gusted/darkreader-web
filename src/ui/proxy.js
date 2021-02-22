ef9d34c288640242d2b017361d3eb13e8406a306
Gusted <williamzijl7@hotmail.com>
2021-02-22 13:56:36 +0100

    Fix query (duckduckgo.com's search)

--- src/ui/proxy.js
+++ src/ui/proxy.js
@@ -4,3 +4,14 @@ document.addEventListener("click", (e) => {
         window.top.location.href = `${window.top.location.href.substring(0, window.top.location.href.indexOf("?") + 1)}url=${document.activeElement.href}`;
     }
 });
+document.addEventListener('submit', (e) => {
+    if (frameElement && document.activeElement && document.activeElement.form && document.activeElement.form.action) {
+        e.preventDefault()
+        if (document.activeElement.form.method === 'post') {
+            // const method = {method: 'post', body: new FormData(document.activeElement.form)};
+            // TO-DO
+        } else {
+            window.top.location.href = `${window.top.location.href.substring(0, window.top.location.href.indexOf("?") + 1)}url=${document.activeElement.form.action}?${new URLSearchParams(new FormData(document.activeElement.form))}`;
+        }
+    }
+})
